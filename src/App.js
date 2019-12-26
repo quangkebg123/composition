e9d62a48534de9cca4b64307357a524668350092
Quang Nguyen Manh (CongNghe-HO) <quang.nguyenmanh@vndirect.com.vn>
2019-12-26 17:15:58 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// import { Message } from "./Message";
+// import { ActionButton } from './ActionButton';
+// import { ThemeSelector } from './ThemeSelector';
+import { GeneralList } from './GeneralList';
+import { SortedList } from "./SortedList";
+import { ProFeature } from "./ProFeature";
+// import { ProController } from "./ProController";
+// import { LogToConsole } from "./LogToConsole";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const ProList = ProController(LogToConsole(SortedList, "Sorted", true, true, true));
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // counter: 0
+            names: ["Zoe", "Bob", "Alice", "Dora", "Joe"],
+            cities: ["London", "New York", "Paris", "Milan", "Boston"],
+            proMode: false
+        }
+    }
+
+    toggleProMode = () => {
+        this.setState({ proMode: !this.state.proMode });
+    }
+
+    render() {
+        return <div className="container-fluid">
+            <div className="row">
+                <div className="col-12 text-center p-2">
+                    <div className="form-check">
+                        <input type="checkbox" className="form-check-input"
+                            value={this.state.proMode}
+                            onChange={this.toggleProMode} />
+                        <label className="form-check-label">Pro Mode</label>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-6">
+                    <GeneralList list={this.state.names}
+                        theme="primary" />
+                </div>
+                <div className="col-6">
+                    <ProFeature pro={this.state.proMode}
+                        render={() => <SortedList list={this.state.names} />}
+                    />
+                </div>
+            </div>
+        </div>
+    }
 }
-
-export default App;
---
+++ src/ProFeature.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export function ProFeature(props) {
+    return function (props) {
+        if (props.pro) {
+            return props.render();
+        } else {
+            return (
+                <h5 className="bg-warning text-white text-center">
+                    This is a Pro Feature
+                </h5>
+            )
+        }
+    }
+}
